7454bb2bf110bcdc85bcd1b9d9c47a0a3b3d7ff5
didiraja <didiraja@hotmail.com>
2018-08-07 16:03:51 -0300

    projeto reiniciado; incluído metodo; component lendo initialState

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import Hello from './Hi';
-import Name from './NameField';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
         
-        <Hello name="Dico" />
-
-        <Name placeholder="Digite seu nome aqui" onChange={this.mudaNome} />
+        <Hello />
 
       </div>
     );
--- src/components/Hi.js
+++ src/components/Hi.js
@@ -4,15 +4,26 @@ export default class Hello extends Component {
 
     constructor() {
         super();
+
         this.state = {
-            nome: 'Nome padrão'
+            name: "Nome Padrão",
+            placeholder: "Digite seu nome aqui"
         }
+
+    }
+
+    mudaNome(e) {
+        console.log(e);
     }
 
     render() {
 
         return (
-            <p id="hello-field" className="h1 text-center py-3 bg-light">Hello {this.state.nome}!</p>
+            <React.Fragment>
+                <p id="hello-field" className="h1 text-center py-3 bg-light">Hello {this.state.name}!</p>
+
+                <input id="campo-value" type="text" className="form-control w-50 text-center mx-auto my-4" placeholder={this.state.placeholder} onKeyDown={this.mudaNome} />
+            </React.Fragment>
         );
     }
 
